43fed398015e9e31ef227a43152511b95a80d4b0
crazywoola <li.zheng@dentsplysirona.com>
2023-03-06 11:21:25 +0800

    🚀

---
+++ dist/module.js
@@ -0,0 +1,23 @@
+import {jsx as $hgUW1$jsx, Fragment as $hgUW1$Fragment} from "react/jsx-runtime";
+import "react";
+
+function $parcel$defineInteropFlag(a) {
+  Object.defineProperty(a, '__esModule', {value: true, configurable: true});
+}
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+var $f70242fa1ecd7637$exports = {};
+
+$parcel$defineInteropFlag($f70242fa1ecd7637$exports);
+
+$parcel$export($f70242fa1ecd7637$exports, "default", () => $f70242fa1ecd7637$export$2e2bcd8739ae039);
+
+
+const $f70242fa1ecd7637$var$ImageCursor = ()=>/*#__PURE__*/ (0, $hgUW1$jsx)((0, $hgUW1$Fragment), {});
+var $f70242fa1ecd7637$export$2e2bcd8739ae039 = $f70242fa1ecd7637$var$ImageCursor;
+
+
+
+
+//# sourceMappingURL=module.js.map
