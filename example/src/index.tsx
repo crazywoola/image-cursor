22b5df85f8681a7ef05e8dffb55e02a1b95c911f
crazywoola <li.zheng@dentsplysirona.com>
2023-03-06 14:34:26 +0800

    🚀

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,9 +2,41 @@ import { createRoot } from "react-dom/client";
 import React from 'react';
 
 const root = createRoot(document.getElementById("app") as HTMLElement);
+
+const data = [
+    { id: 1, title: 'First item', desc: 'A description of the first item' },
+    { id: 2, title: 'Second item', desc: 'A description of the second item' },
+    { id: 3, title: 'Third item', desc: 'A description of the third item' },
+];
+
+
+const Table = ({ data }) => {
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>TITLE</th>
+                    <th>DESC</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((row) => (
+                    <tr key={row.id}>
+                        <td>{row.id}</td>
+                        <td>{row.title}</td>
+                        <td>{row.desc}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
 const App = () => {
-    return <div>
-        Hello
+    return <div className="container">
+        <div className="table-container">
+            <Table data={data} />
+        </div>
     </div>
 }
 root.render(<App />);
